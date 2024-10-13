2dad5e7d31d2bb1345d307f19ff5f6c13f1c9cbe
anubhavhajela4 <anubhavhajela4@gmail.com>
2024-10-13 17:01:02 +0530

    Added animating sprite which can move

--- js/game.js
+++ js/game.js
@@ -1,11 +1,60 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
-canvas.width = 1024;
-canvas.height = 576;
+canvas.width = 1300;
+canvas.height = 600;
 const gravity = 0.2;
 class Sprite {
-    constructor({position,velocity,color='red',offset})     {
+    constructor({position,imageSrc,scale=1,framesMax=1,offset={x:0,y:0}})   {
         this.position = position ;
+        this.width=50;
+        this.height = 150;
+        this.image = new Image();
+        this.image.src = imageSrc;
+        this.scale=scale;
+        this.framesMax=framesMax;
+        this.framesCurrent=0;
+        this.framesElapsed=0;
+        this.framesHold=1;
+        this.offset=offset
+    }
+    draw () {
+        c.drawImage(
+            this.image,
+            this.framesCurrent * (this.image.width /this.framesMax),
+            0,
+            this.image.width/this.framesMax,
+            this.image.height,
+            this.position.x-this.offset.x,
+            this.position.y-this.offset.y,
+            (this.image.width/this.framesMax) * this.scale,
+            this.image.height * this.scale
+        )
+    }
+    animateFrames () {
+        this.framesElapsed++;
+        if(this.framesElapsed % this.framesHold ===0) {
+        if(this.framesCurrent < this.framesMax -1) {
+        this.framesCurrent++;
+        }
+        else {
+            this.framesCurrent=0;
+        }
+    }
+    }
+    update() {
+        this.draw();
+        this.animateFrames();
+    }
+}
+class Fighter extends Sprite {
+    constructor({position,velocity,color='red', imageSrc,scale=1,framesMax=1,offset={x:0,y:0}})   {
+        super({
+            position,
+            imageSrc,
+            scale,
+            framesMax,
+            offset,
+        });
         this.velocity = velocity ;
         this.width=50;
         this.height = 150;
@@ -22,18 +71,14 @@ class Sprite {
         this.color=color;
         this.isAttacking;
         this.health=100;
+        this.framesCurrent=0;
+        this.framesElapsed=0;
+        this.framesHold=6;
     }
-    draw () {
-        c.fillStyle=this.color;
-        c.fillRect(this.position.x,this.position.y,this.width,this.height);
-        //attack box 
-         if(this.isAttacking) {
-            c.fillStyle='green';
-            c.fillRect(this.attackBox.position.x,this.attackBox.position.y,this.attackBox.width,this.attackBox.height);
-         }
-    }
+
     update() {
         this.draw();
+        this.animateFrames();
         this.attackBox.position.x=this.position.x+this.attackBox.offset.x;
         this.attackBox.position.y=this.position.y;
         this.position.x+=this.velocity.x;
@@ -52,7 +97,7 @@ class Sprite {
     }
 }
 
-const player = new Sprite ( {
+const player = new Fighter ( {
     position: {
     x:0,
     y:0
@@ -64,10 +109,17 @@ const player = new Sprite ( {
     offset: {
         x:0,
         y:0
+    },
+    imageSrc:'../assets/player/Idle.png',
+    framesMax:10,
+    scale:3,
+    offset: {
+        x:0,
+        y:130
     }
 });
 
-const enemy = new Sprite ( {
+const enemy = new Fighter ( {
     position: {
     x:400,
     y:100
@@ -116,7 +168,7 @@ function animate () {
     window.requestAnimationFrame(animate);
     c.clearRect(0,0,canvas.width,canvas.height);
     player.update();
-    enemy.update();
+    // enemy.update();
     player.velocity.x=0;
     enemy.velocity.x=0;
     //player movement
